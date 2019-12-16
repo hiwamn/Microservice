a07919bd5e6d1e3d96e25f17b5e2f20dbdde7793
unknown <Hirad@DESKTOP-NF0BER5.kargozari22.ir>
2019-12-16 22:43:33 +0330

    webhost

---
+++ src/Actio.Common/Events/CreateUserRejected.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actio.Common.Events
+{
+    public class CreateUserRejected : IRejectedEvent
+    {
+        public string Reason { get; }
+
+        public string Code { get; }
+        public string Email { get; }
+
+        protected CreateUserRejected()
+        { 
+        }
+
+        public CreateUserRejected(string email,string reason,string code)
+        {
+            Email = email;
+            Reason = reason;
+            Code = code;
+        }
+    }
+}
---
+++ src/Actio.Common/Services/ServiceHost.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actio.Common.Services
+{
+    public class ServiceHost : IServiceHost
+    {
+        private readonly IWebHost _webHost;
+
+        public ServiceHost(IWebHost webHost)
+        {
+            this._webHost = webHost;
+        }
+        public void Run() => _webHost.Run();
+
+
+    }
+}
