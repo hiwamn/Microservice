4e66e257fa7a551cf03195022f5b76843e0168fa
هیوا محمودی نژاد <3849824527@kargozari22.ir>
2019-12-17 16:50:46 +0330

    0926

--- src/Actio.Common/Services/ServiceHost.cs
+++ src/Actio.Common/Services/ServiceHost.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Actio.Common.Commands;
+using Actio.Common.Events;
+using Actio.Common.RabbitMq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using RawRabbit;
 
 namespace Actio.Common.Services
 {
@@ -15,6 +18,78 @@ namespace Actio.Common.Services
         }
         public void Run() => _webHost.Run();
 
+        
+
+        public static HostBuilder Create<TStartup>(string[] args) where TStartup : class
+        {
+            var config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+            var webHostBuilder = WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(config)
+                .UseStartup<TStartup>();
+
+            return new HostBuilder(webHostBuilder.Build());
+        }
+
+        public abstract class BuilderBase
+        {
+            public abstract ServiceHost Build();
+        }
+
+        public class HostBuilder : BuilderBase
+        {
+            public IWebHost _webHost { get; }
+
+            public IBusClient _bus;
+            public HostBuilder(IWebHost webHost)
+            {
+                this._webHost = webHost;
+            }
+
+            public BusBuilder UseRabbitMq()
+            {
+                _bus = (IBusClient)_webHost.Services.GetService(typeof(IBusClient));
+                return new BusBuilder(_webHost, _bus);
+            }
+
+
+            public override ServiceHost Build()
+            {
+                return new ServiceHost(_webHost);
+            }
+        }
+
+        public class BusBuilder : BuilderBase
+        {
+            public IWebHost _webHost { get; }
+
+            public IBusClient _bus;
+
+            public BusBuilder(IWebHost webHost, IBusClient bus)
+            {
+                _webHost = webHost;
+                _bus = bus;
+            }
+            public override ServiceHost Build()
+            {
+                return new ServiceHost(_webHost);
+            }
+
+            public BusBuilder SubscribeToCommand<TCommand>() where TCommand : ICommand
+            {
+                var handler = (ICommandHandler<TCommand>)_webHost.Services.GetService(typeof(ICommandHandler<TCommand>));
+                _bus.WithCommandHandlerAsync(handler);
+                return this;
+            }
+            public BusBuilder SubscribeToEvent<TEvent>() where TEvent : IEvent
+            {
+                var handler = (IEventHandler<TEvent>)_webHost.Services.GetService(typeof(IEventHandler<TEvent>));
+                _bus.WithEventHandlerAsync(handler);
+                return this;
+            }
+        }
 
     }
 }
